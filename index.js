547ed420191176141a4a70bd5f9a228a6671021c
Mirza Mukarram <ahmedmukarram6@gmail.com>
2025-04-29 22:56:46 +0500

    Simple Chat app using socket.io

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const path=require('path')
+
+const path= require('path')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.resolve('./public')))
 
+app.get('/', (req, res) => {
+  res.sendFile('/public/index.html');
+});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
   });
-  
-app.get('/', (req, res) => {
-  res.sendFile('/public/index.html');
-});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
